82045ab23c910b168f9b5295a469d1e5bbad2579
Yuvarajaa <16523925+yuva-dev@users.noreply.github.com>
2023-09-12 22:02:42 +0530

    launch

--- buildAndLaunch.sh
+++ buildAndLaunch.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 git pull origin master
 
-docker stop gps
-docker build -t gps .
-docker run -p 1800:1800 --name gps gps:latest &
+docker stack deploy --with-registry-auth -c docker-compose.yaml gps
