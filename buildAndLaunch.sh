cfef10b4529c871e72b09d2c9dbd5e56aa000d3c
Yuvarajaa <16523925+yuva-dev@users.noreply.github.com>
2023-09-13 13:13:12 +0530

    Quick commit

--- buildAndLaunch.sh
+++ buildAndLaunch.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 git pull origin master
 
-docker stack deploy --with-registry-auth -c docker-compose.yaml gps
+# docker stack deploy --with-registry-auth -c docker-compose.yaml gps
+
+docker stack deploy --with-registry-auth -c compose-lb.yaml lb
+docker stack deploy --with-registry-auth -c compose-gps.yaml gps
