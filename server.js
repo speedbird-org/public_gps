dc9c67e798905dd51678136426a2fc93ec509b4a
Yuvarajaa <16523925+yuva-dev@users.noreply.github.com>
2023-09-15 07:56:00 +0530

    Quick commit

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const server = net.createServer((socket) => {
 
 
 server.listen(port, () => {
-    log(`TCP Stream Server listening on port ${port}. Name:${SERVER_NAME}`);
+    log(`Version 3: TCP Stream Server listening on port ${port}. Name:${SERVER_NAME}`);
 });
 
 function log(str){
