b9f77bd185fe88dbd90cbe8cd0bfa8d5f659973b
Yuvarajaa <16523925+yuva-dev@users.noreply.github.com>
2023-10-08 18:30:02 +0530

    Quick commit

--- stressTest.js
+++ stressTest.js
@@ -1,6 +1,6 @@
 const net = require('net');
 
-const SERVER_IP = '142.93.221.10'; //docker swarm entry point
+const SERVER_IP = '139.59.44.250'; //docker swarm entry point
 const SERVER_PORT = 1800;
 
 
