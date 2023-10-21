e112e0a6d843ca3d3450785f1f80b55ecc34fcfb
Yuvarajaa <16523925+yuva-dev@users.noreply.github.com>
2023-10-21 13:58:05 +0530

    Quick commit

--- server.js
+++ server.js
@@ -2,13 +2,14 @@ const net = require('net');
 const port = 1800;
 
 const SERVER_NAME = process.env.TASK_ID;
+const VERSION = `V7: Memory Leak: `
 
 let memoryLeak = [];
 
 const server = net.createServer((socket) => {
     // This function will be called when a client connects to the server.
     log('Client connected');
-    socket.write(`Welcome to kube TCP stream. Connected to Server: ${SERVER_NAME}`);
+    socket.write(`${VERSION} Connected to Server: ${SERVER_NAME}`);
 
     // Handle incoming data from the client
     socket.on('data', (data) => {
@@ -30,7 +31,7 @@ const server = net.createServer((socket) => {
 
 
 server.listen(port, () => {
-    log(`Version 7:TCP Stream Server listening on port ${port}. Name: ${SERVER_NAME}`);
+    log(`${VERSION} Listening on port ${port}. Name: ${SERVER_NAME}`);
 });
 
 function log(str) {
