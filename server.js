da7b1a11e19a7f20cccb3a7d32bce56c14e0ec7b
Yuvarajaa <16523925+yuva-dev@users.noreply.github.com>
2023-10-21 20:36:10 +0530

    Quick commit

--- server.js
+++ server.js
@@ -1,39 +1,45 @@
 const net = require('net');
-const port = 1800;
 
-const SERVER_NAME = process.env.TASK_ID;
-const VERSION = `V7: Memory Leak: `
+const SERVER_ID = new Date().getTime();
+const PORT_COUNT = process.env.PORT_COUNT ? parseInt(process.env.PORT_COUNT) : 1;
+const MSG = `V8 TCP Server: `
+console.log(`PORT_COUNT: ${PORT_COUNT}`);
 
-let memoryLeak = [];
-
-const server = net.createServer((socket) => {
-    // This function will be called when a client connects to the server.
-    log('Client connected');
-    socket.write(`${VERSION} Connected to Server: ${SERVER_NAME}`);
-
-    // Handle incoming data from the client
-    socket.on('data', (data) => {
-        log(`Received data: ${data}`);
-        socket.write(data);
-        memoryLeak.push(data);
-    });
-
-    // Handle the client disconnecting
-    socket.on('end', () => {
-        log('Client disconnected');
-    });
-
-    // Handle errors
-    socket.on('error', (err) => {
-        log(`Error: ${err}`);
-    });
-});
-
-
-server.listen(port, () => {
-    log(`${VERSION} Listening on port ${port}. Name: ${SERVER_NAME}`);
-});
 
 function log(str) {
     console.log(`${new Date().toISOString()}: ${str}`);
+}
+
+function createServerForPort(port) {
+    const server = net.createServer((socket) => {
+        // This function will be called when a client connects to the server.
+        log('Client connected');
+        socket.write(`${MSG} Connected to Server ID: ${SERVER_ID}`);
+
+        // Handle incoming data from the client
+        socket.on('data', (data) => {
+            log(`Received data: ${data}`);
+            socket.write(data);
+        });
+
+        // Handle the client disconnecting
+        socket.on('end', () => {
+            log(`Client disconnected. ID: ${SERVER_ID}`);
+        });
+
+        // Handle errors
+        socket.on('error', (err) => {
+            log(`Error: ${err}`);
+        });
+    });
+
+    server.listen(port, () => {
+        log(`${MSG} Listening on port: ${port} ID: ${SERVER_ID}`);
+    });
+
+}
+
+
+for (let index = 0; index < PORT_COUNT; index++) {
+    createServerForPort(1800 + index);
 }
