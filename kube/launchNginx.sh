add4e022efe10536d8b9e396b326645bca87f1b5
Yuvarajaa <16523925+yuva-dev@users.noreply.github.com>
2023-10-29 15:15:43 +0530

    Quick commit

--- kube/launchNginx.sh
+++ kube/launchNginx.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 # docker stack deploy --with-registry-auth -c docker-compose-nginx.yaml gps
+
+docker-compose -f docker-compose-nginx.yaml down --remove-orphans
 docker-compose -f docker-compose-nginx.yaml up -d
