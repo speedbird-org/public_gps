78e022dd8109f4f0abd85dfb577f4087c647cfda
srisudhan <srisudhan.dev@gmail.com>
2024-03-21 10:57:12 +0530

    added test_api

--- gke/server.js
+++ gke/server.js
@@ -3,8 +3,14 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 3000;
 
+app.use(express.json());
+
 app.get('/', (req, res) => {
   res.send('Hello, world!');
+});
+
+app.post('/test_api',(req,res)=>{
+  res.send("Hello , Running test api");
 });
 
 app.listen(port, () => {
